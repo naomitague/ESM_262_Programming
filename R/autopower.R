c1bd2227a1d2dbc5c5ca63b65391c6c6718c80ef
Naomi Tague <ctague@bren.ucsb.edu>
2022-02-27 16:54:03 -0800

    added control structures

--- R/autopower.R
+++ R/autopower.R
@@ -1,9 +1,9 @@
 
 #' Power Required by Speed
 #'
-#' This function determines the power required to keep a vehicle moving at 
+#' This function determines the power required to keep a vehicle moving at
 #' a given speed
-#' @param cdrag coefficient due to drag default=0.3 
+#' @param cdrag coefficient due to drag default=0.3
 #' @param crolling coefficient due to rolling/friction default=0.015
 #' @param v vehicle speed (m/2)
 #' @param m vehicle mass (kg)
@@ -12,7 +12,8 @@
 #' @param pair (kg/m3) default =1.2
 #' @return power (W)
 
-autopower = function(V, m, A, cdrag=0.3, crolling=0.015,pair=1.2,g=9.8) {
+autopower = function(V, m, l, w, cdrag=0.3, crolling=0.015,pair=1.2,g=9.8) {
+  A = l*w
   P = crolling*m*g*V + 1/2*A*pair*cdrag*V**3
   return(P)
 }
--- R/classify_lake.R
+++ R/classify_lake.R
@@ -1,5 +1,5 @@
 #' classify_lake
-#' 
+#'
 #' compute net present value
 #' @param nutrient concentraion (mg/L) (at least 5 days)
 #' @param temperature (C) (at least 5 days)
@@ -11,21 +11,21 @@
 
 classify_lake = function(nutrient, temperature, nutrient_threshold_med=5,
                          nutrient_threshold_high=8, temperature_threshold=10) {
-  
+
   # start with some error checking
   if (length(nutrient) < 5)
     return("Not enough nutrient measurements, at least 5 are needed")
-  
+
   if (length(temperature) < 5)
       return("Not enought temperature measurements, at least 5 are needed")
 
 
-  # a for loop is useful here
+  # a while loop is useful here
   numb10 = 0
   i=1
-  # we use while here because we want to exit our loop any time we get more than 5 days with 
+  # we use while here because we want to exit our loop any time we get more than 5 days with
   # air temperature greater than the threshold
-  
+
   while ( (numb10 < 5) && (i <= length(temperature))) {
       if (temperature[i] > temperature_threshold)
         # we have another day with greater than 10 so accumulate
@@ -33,22 +33,24 @@ classify_lake = function(nutrient, temperature, nutrient_threshold_med=5,
       else
         # we have to start over
         numb10 = 0
-      # remember with while loops we need to increment our counter 
+      # remember with while loops we need to increment our counter
       i = i+1
+
   }
-  
-  # compute the mean nutrient 
+
+  # compute the mean nutrient
   mean_nutrient = mean(nutrient)
-  
+
   # only high or med if temperature has been 10 degrees for more than 5 days
   if (numb10 >= 5) {
    risk= case_when ( mean_nutrient < nutrient_threshold_med ~ "low",
-                     mean_nutrient >= nutrient_threshold_med & 
+                     mean_nutrient >= nutrient_threshold_med &
                        mean_nutrient < nutrient_threshold_high ~ "med",
                 mean_nutrient >= nutrient_threshold_high ~ "high" )
   } else
     risk = "low"
-  
-  return(risk)
+
+
+  return(list(risk=risk, mean_nutrient=mean_nutrient))
 }
 
